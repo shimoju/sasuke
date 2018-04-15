157979f2cc442ec5738f87698f60b3b2c19dc48f
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2018-04-15 14:37:54 +0900

    Add loginParams test

--- src/kinnosuke.test.js
+++ src/kinnosuke.test.js
@@ -6,7 +6,7 @@ beforeEach(() => {
   client = new Kinnosuke('foo', 'bar', 'p@ssw0rd');
 });
 
-describe('baseUrl', () => {
+describe('#baseUrl', () => {
   describe('引数を省略したとき', () => {
     test('デフォルト値が設定される', () => {
       expect(client.baseUrl).toBe('https://www.4628.jp');
@@ -21,5 +21,11 @@ describe('baseUrl', () => {
     test('指定した値が設定される', () => {
       expect(client.baseUrl).toBe('https://example.com');
     });
+  });
+});
+
+describe('#loginParams', () => {
+  test('ログインに必要なパラメータをapplication/x-www-form-urlencoded形式の文字列で返す', () => {
+    expect(client.loginParams()).toBe('module=login&y_companycd=foo&y_logincd=bar&password=p%40ssw0rd');
   });
 });
