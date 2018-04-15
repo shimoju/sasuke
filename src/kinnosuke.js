53d2db67deaaeec558bc624f462a815ab8681663
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2018-04-15 13:02:33 +0900

    Add Kinnosuke#getTimeSheet

--- src/kinnosuke.js
+++ src/kinnosuke.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import axiosCookieJarSupport from 'axios-cookiejar-support';
 import tough from 'tough-cookie';
+import { JSDOM } from 'jsdom';
+import { URLSearchParams } from 'url';
 
 export default class Kinnosuke {
   constructor(companyId, loginId, password, baseUrl = 'https://www.4628.jp') {
@@ -18,5 +20,42 @@ export default class Kinnosuke {
     });
     axiosCookieJarSupport(this.http);
   }
+
+  async getTimeSheet() {
+    const doc = await this.getWithLogin('/?module=timesheet&action=browse');
+    const table = doc.getElementById('total_list0');
+
+    return table;
+  }
+
+  async getWithLogin(path) {
+    const firstTry = await this.http.get(path);
+    const loginButtonId = 'id_passlogin';
+
+    if (firstTry.data.includes(loginButtonId)) {
+      const login = await this.login();
+      const retry = await this.http.get(path);
+
+      return (new JSDOM(retry.data)).window.document;
+    } else {
+      return (new JSDOM(firstTry.data)).window.document;
+    }
+  }
+
+  async login() {
+    const response = await this.http.post('/', this.loginParams());
+
+    return response;
+  }
+
+  loginParams() {
+    const params = new URLSearchParams({
+      module: 'login',
+      y_companycd: this.companyId,
+      y_logincd: this.loginId,
+      password: this.password,
+    });
+
+    return params.toString();
   }
 };
