99f9287d6bcf0cf68fc353f70c5c10538c3f1897
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2018-04-15 14:23:58 +0900

    Add baseUrl test

--- src/kinnosuke.test.js
+++ src/kinnosuke.test.js
@@ -1,6 +1,25 @@
 import Kinnosuke from './kinnosuke';
 
-test('default baseUrl', () => {
-  const client = new Kinnosuke('foo', 'bar', 'password');
-  expect(client.baseUrl).toBe('https://www.4628.jp');
+let client;
+
+beforeEach(() => {
+  client = new Kinnosuke('foo', 'bar', 'p@ssw0rd');
+});
+
+describe('baseUrl', () => {
+  describe('引数を省略したとき', () => {
+    test('デフォルト値が設定される', () => {
+      expect(client.baseUrl).toBe('https://www.4628.jp');
+    });
+  });
+
+  describe('引数を指定したとき', () => {
+    beforeEach(() => {
+      client = new Kinnosuke('foo', 'bar', 'p@ssw0rd', 'https://example.com');
+    });
+
+    test('指定した値が設定される', () => {
+      expect(client.baseUrl).toBe('https://example.com');
+    });
+  });
 });
